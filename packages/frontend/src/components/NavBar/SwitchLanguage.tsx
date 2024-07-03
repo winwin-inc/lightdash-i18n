f94de549ae5142b76fd810700f34f0bd771c572e
heora <yueluo.yang@gmail.com>
2024-07-03 06:32:44 +0000

    feat: add switch language component

---
+++ packages/frontend/src/components/NavBar/SwitchLanguage.tsx
@@ -0,0 +1,36 @@
+import { Select } from '@mantine/core';
+import { Suspense, type FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const normalizedLanguage = (language: string) => {
+    return language.toLowerCase().split('-')[0];
+};
+
+const languages = [
+    { value: 'en', label: 'English' },
+    { value: 'zh', label: '中文' },
+];
+
+const SwitchLanguage: FC = () => {
+    const { i18n } = useTranslation();
+
+    const language = normalizedLanguage(i18n.language);
+
+    const handleLanguageChange = async (lang: string) => {
+        await i18n.changeLanguage(lang);
+    };
+
+    return (
+        <Suspense fallback={null}>
+            <Select
+                size="xs"
+                value={language}
+                data={languages}
+                style={{ width: 100 }}
+                onChange={handleLanguageChange}
+            />
+        </Suspense>
+    );
+};
+
+export default SwitchLanguage;
