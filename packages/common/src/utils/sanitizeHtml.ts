035a5e89bc3d16e5ea50de295fb837a08f018bc4
yzqzy <yangzhiqiang@winwin-inc.com>
2025-08-20 09:15:07 +0800

    perf: improve formatting of allowed styles in sanitizeHtml utility for better readability

--- packages/common/src/utils/sanitizeHtml.ts
+++ packages/common/src/utils/sanitizeHtml.ts
@@ -62,16 +62,21 @@ const allowedTextStylingProperties: NonNullable<
  * style attribute in the div tag.
  */
 const allowedDivStyleProperties: NonNullable<
-    sanitize.IOptions['allowedStyles']>[string] = {
+    sanitize.IOptions['allowedStyles']
+>[string] = {
     // layout properties
     display: [/^(?:block|inline|inline-block|flex|grid|none|contents)$/],
     position: [/^(?:static|relative|absolute|fixed|sticky)$/],
     'z-index': [/^-?\d+$/, /^auto$/],
-    
+
     // Flexbox properties
-    'justify-content': [/^(?:flex-start|flex-end|center|space-between|space-around|space-evenly)$/],
+    'justify-content': [
+        /^(?:flex-start|flex-end|center|space-between|space-around|space-evenly)$/,
+    ],
     'align-items': [/^(?:flex-start|flex-end|center|baseline|stretch)$/],
-    'align-content': [/^(?:flex-start|flex-end|center|space-between|space-around|space-evenly|stretch)$/],
+    'align-content': [
+        /^(?:flex-start|flex-end|center|space-between|space-around|space-evenly|stretch)$/,
+    ],
     'align-self': [/^(?:auto|flex-start|flex-end|center|baseline|stretch)$/],
     'flex-direction': [/^(?:row|row-reverse|column|column-reverse)$/],
     'flex-wrap': [/^(?:nowrap|wrap|wrap-reverse)$/],
@@ -79,16 +84,16 @@ const allowedDivStyleProperties: NonNullable<
     'flex-shrink': [/^\d+$/],
     'flex-basis': [/^\d+(?:px|em|rem|%)$/, /^auto$/],
     flex: [/^(?:none|auto|\d+(?:\s+\d+)?(?:\s+\d+(?:px|em|rem|%)|auto)?)$/],
-    
+
     // Grid properties
-    'grid-template-columns': [/^[a-zA-Z0-9\s\-\.]+$/],
-    'grid-template-rows': [/^[a-zA-Z0-9\s\-\.]+$/],
-    'grid-column': [/^[a-zA-Z0-9\s\-\.]+$/],
-    'grid-row': [/^[a-zA-Z0-9\s\-\.]+$/],
+    'grid-template-columns': [/^[a-zA-Z0-9s\-.]+$/],
+    'grid-template-rows': [/^[a-zA-Z0-9s\-.]+$/],
+    'grid-column': [/^[a-zA-Z0-9s\-.]+$/],
+    'grid-row': [/^[a-zA-Z0-9s\-.]+$/],
     'grid-gap': [/^\d+(?:px|em|rem|%)$/],
     'grid-column-gap': [/^\d+(?:px|em|rem|%)$/],
     'grid-row-gap': [/^\d+(?:px|em|rem|%)$/],
-    
+
     // Size properties
     width: [/^\d+(?:px|em|rem|%|vw|vh)$/, /^auto$/],
     height: [/^\d+(?:px|em|rem|%|vh|vw)$/, /^auto$/],
@@ -96,43 +101,89 @@ const allowedDivStyleProperties: NonNullable<
     'min-height': [/^\d+(?:px|em|rem|%|vh|vw)$/],
     'max-width': [/^\d+(?:px|em|rem|%|vw|vh)$/, /^none$/],
     'max-height': [/^\d+(?:px|em|rem|%|vh|vw)$/, /^none$/],
-    
+
     // Padding properties
-    padding: [/^\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/],
+    padding: [
+        /^\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+    ],
     'padding-top': [/^\d+(?:px|em|rem|%)$/],
     'padding-right': [/^\d+(?:px|em|rem|%)$/],
     'padding-bottom': [/^\d+(?:px|em|rem|%)$/],
     'padding-left': [/^\d+(?:px|em|rem|%)$/],
-    
+
     // Margin properties
-    margin: [/^\d+(?:px|em|rem|%)$/, /^auto$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/],
+    margin: [
+        /^\d+(?:px|em|rem|%)$/,
+        /^auto$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+    ],
     'margin-top': [/^\d+(?:px|em|rem|%)$/, /^auto$/],
     'margin-right': [/^\d+(?:px|em|rem|%)$/, /^auto$/],
     'margin-bottom': [/^\d+(?:px|em|rem|%)$/, /^auto$/],
     'margin-left': [/^\d+(?:px|em|rem|%)$/, /^auto$/],
-    
+
     // Border properties
-    border: [/^(?:none|initial|inherit)$/, /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/],
-    'border-width': [/^\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/],
-    'border-style': [/^(?:solid|dashed|dotted|double|groove|ridge|inset|outset|none)$/],
+    border: [
+        /^(?:none|initial|inherit)$/,
+        /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/,
+    ],
+    'border-width': [
+        /^\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+    ],
+    'border-style': [
+        /^(?:solid|dashed|dotted|double|groove|ridge|inset|outset|none)$/,
+    ],
     'border-color': colorRegexes,
-    'border-radius': [/^\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/, /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/],
+    'border-radius': [
+        /^\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+        /^\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)\s+\d+(?:px|em|rem|%)$/,
+    ],
     'border-top-left-radius': [/^\d+(?:px|em|rem|%)$/],
     'border-top-right-radius': [/^\d+(?:px|em|rem|%)$/],
     'border-bottom-left-radius': [/^\d+(?:px|em|rem|%)$/],
     'border-bottom-right-radius': [/^\d+(?:px|em|rem|%)$/],
-    'border-right': [/^(?:none|initial|inherit)$/, /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/],
-    'border-left': [/^(?:none|initial|inherit)$/, /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/],
-    'border-top': [/^(?:none|initial|inherit)$/, /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/],
-    'border-bottom': [/^(?:none|initial|inherit)$/, /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/],
-    
+    'border-right': [
+        /^(?:none|initial|inherit)$/,
+        /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/,
+    ],
+    'border-left': [
+        /^(?:none|initial|inherit)$/,
+        /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/,
+    ],
+    'border-top': [
+        /^(?:none|initial|inherit)$/,
+        /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/,
+    ],
+    'border-bottom': [
+        /^(?:none|initial|inherit)$/,
+        /^\d+(?:px|em|rem|%)\s+(?:solid|dashed|dotted|double|groove|ridge|inset|outset)\s+[a-zA-Z#0-9]+$/,
+    ],
+
     // Background properties
     'background-color': colorRegexes,
     'background-image': [/^url\(['"]?[^'"]*['"]?\)$/, /^none$/],
-    'background-size': [/^(?:cover|contain|auto|initial|inherit)$/, /^\d+(?:px|em|rem|%|auto)(?:\s+\d+(?:px|em|rem|%|auto))?$/],
-    'background-position': [/^(?:left|right|top|bottom|center)$/, /^\d+(?:px|em|rem|%)(?:\s+\d+(?:px|em|rem|%))?$/],
-    'background-repeat': [/^(?:repeat|no-repeat|repeat-x|repeat-y|space|round)$/],
-    
+    'background-size': [
+        /^(?:cover|contain|auto|initial|inherit)$/,
+        /^\d+(?:px|em|rem|%|auto)(?:\s+\d+(?:px|em|rem|%|auto))?$/,
+    ],
+    'background-position': [
+        /^(?:left|right|top|bottom|center)$/,
+        /^\d+(?:px|em|rem|%)(?:\s+\d+(?:px|em|rem|%))?$/,
+    ],
+    'background-repeat': [
+        /^(?:repeat|no-repeat|repeat-x|repeat-y|space|round)$/,
+    ],
+
     // Text properties
     color: colorRegexes,
     'font-size': [/^\d+(?:px|em|rem|%)$/],
@@ -144,23 +195,34 @@ const allowedDivStyleProperties: NonNullable<
     'white-space': [/^(?:normal|nowrap|pre|pre-wrap|pre-line)$/],
     'word-wrap': [/^(?:normal|break-word)$/],
     'overflow-wrap': [/^(?:normal|break-word|break-all)$/],
-    
+
     // Transform properties
     transform: [/^(?:none|translate|rotate|scale|skew|matrix)(?:\([^)]*\))?$/],
-    'transform-origin': [/^(?:left|right|top|bottom|center)$/, /^\d+(?:px|em|rem|%)(?:\s+\d+(?:px|em|rem|%))?$/],
-    
+    'transform-origin': [
+        /^(?:left|right|top|bottom|center)$/,
+        /^\d+(?:px|em|rem|%)(?:\s+\d+(?:px|em|rem|%))?$/,
+    ],
+
     // Transition and animation properties
     transition: [/^[a-zA-Z-]+\s+\d+(?:ms|s)(?:\s+[a-zA-Z-]+)?$/],
     'transition-duration': [/^\d+(?:ms|s)$/],
-    'transition-timing-function': [/^(?:ease|linear|ease-in|ease-out|ease-in-out)$/],
-    
+    'transition-timing-function': [
+        /^(?:ease|linear|ease-in|ease-out|ease-in-out)$/,
+    ],
+
     // Other common properties
     opacity: [/^[01]?\.?\d+$/],
-    'box-shadow': [/^(?:none|inset\s+)?\d+(?:px|em|rem)\s+\d+(?:px|em|rem)(?:\s+\d+(?:px|em|rem))?(?:\s+[a-zA-Z#0-9]+)?$/],
-    'text-shadow': [/^(?:none|\d+(?:px|em|rem)\s+\d+(?:px|em|rem)(?:\s+\d+(?:px|em|rem))?(?:\s+[a-zA-Z#0-9]+)?)$/],
+    'box-shadow': [
+        /^(?:none|inset\s+)?\d+(?:px|em|rem)\s+\d+(?:px|em|rem)(?:\s+\d+(?:px|em|rem))?(?:\s+[a-zA-Z#0-9]+)?$/,
+    ],
+    'text-shadow': [
+        /^(?:none|\d+(?:px|em|rem)\s+\d+(?:px|em|rem)(?:\s+\d+(?:px|em|rem))?(?:\s+[a-zA-Z#0-9]+)?)$/,
+    ],
     cursor: [/^(?:auto|default|pointer|text|wait|move|not-allowed|help)$/],
     'user-select': [/^(?:auto|none|text|all)$/],
-    'pointer-events': [/^(?:auto|none|all|fill|stroke|visible|visibleFill|visibleStroke|painted|fill|stroke|all)$/],
+    'pointer-events': [
+        /^(?:auto|none|all|fill|stroke|visible|visibleFill|visibleStroke|painted|fill|stroke|all)$/,
+    ],
 };
 
 /**
@@ -188,7 +250,7 @@ export const HTML_SANITIZE_MARKDOWN_TILE_RULES: sanitize.IOptions = {
         'iframe',
         'img',
     ],
-    
+
     allowedAttributes: {
         ...HTML_SANITIZE_DEFAULT_RULES.allowedAttributes,
 
@@ -208,18 +270,19 @@ export const HTML_SANITIZE_MARKDOWN_TILE_RULES: sanitize.IOptions = {
         ),
     },
 
-    allowedStyles: Object.fromEntries(
-        [
-            ...tagNamesAllowingTextStyling.map((tagName) => [
-                tagName,
-                allowedTextStylingProperties,
-            ]),
-            ...tagNamesAllowingDivStyle.map((tagName) => [
-                tagName,
-                allowedDivStyleProperties,
-            ]),
-        ],
-    ),
+    allowedStyles: Object.fromEntries([
+        ...tagNamesAllowingTextStyling.map((tagName) => [
+            tagName,
+            allowedTextStylingProperties,
+        ]),
+        ...tagNamesAllowingDivStyle.map((tagName) => [
+            tagName,
+            allowedDivStyleProperties,
+        ]),
+    ]) as Record<
+        string,
+        NonNullable<sanitize.IOptions['allowedStyles']>[string]
+    >,
 };
 
 export const sanitizeHtml = (
