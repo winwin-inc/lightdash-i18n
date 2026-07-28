df2f8ba1e223aa11d6330638cfa3e6e2485bca46
杨志强 <yangzhiqiang@brandct.com>
2026-07-28 09:25:40 +0800

    feat: 更新日期过滤器提示信息为“开始月”并添加新选项

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -2745,6 +2745,7 @@ const models: TsoaRoute.Models = {
             'notInTheCurrent',
             'inBetween',
             'notInBetween',
+            'fromStartToLatestMonth',
         ],
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
